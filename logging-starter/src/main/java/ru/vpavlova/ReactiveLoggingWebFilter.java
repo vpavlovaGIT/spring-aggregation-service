5af7721f3497c182e6675b50bf2adc3a6a0649b3
Victoria Pavlova <vikkkapaw@mail.ru>
2025-07-30 12:31:12 +0300

    Класс ReactiveLoggingWebFilter.

--- logging-starter/src/main/java/ru/vpavlova/ReactiveLoggingWebFilter.java
+++ logging-starter/src/main/java/ru/vpavlova/ReactiveLoggingWebFilter.java
@@ -1,2 +1,22 @@
-package ru.vpavlova;public class ReactiveLoggingWebFilter {
+package ru.vpavlova;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+public class ReactiveLoggingWebFilter implements WebFilter {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        log.info("Incoming WebFlux request: {} {}",
+                exchange.getRequest().getMethod(),
+                exchange.getRequest().getURI());
+
+        return chain.filter(exchange)
+                .doOnSuccess(aVoid -> log.info("Outgoing WebFlux response: {}",
+                        exchange.getResponse().getStatusCode()));
+    }
 }
