dfb2a9f654ce29e979481045a949d1b2cc459267
Victoria Pavlova <vikkkapaw@mail.ru>
2025-09-05 17:17:42 +0300

    В классе ProcessedEvent исправлены проблемы с аннотацией @Data в JPA-сущности.

--- service-aggregation/src/main/java/ru/vpavlova/aggregation/entity/ProcessedEvent.java
+++ service-aggregation/src/main/java/ru/vpavlova/aggregation/entity/ProcessedEvent.java
@@ -1,15 +1,27 @@
 package ru.vpavlova.aggregation.entity;
 
-import jakarta.persistence.*;
-import lombok.Data;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 
-@Data
+@Getter
+@Setter
 @Entity
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString(onlyExplicitlyIncluded = true)
 public class ProcessedEvent {
+
     @Id
+    @EqualsAndHashCode.Include
     private String eventId;
 
+    @ToString.Include
     private LocalDateTime processedAt = LocalDateTime.now();
 }
