5eedeab8258941d668a0ef076942a8e6ccfbeecf
Victoria Pavlova <vikkkapaw@mail.ru>
2025-09-06 20:16:44 +0300

    Исправлена реализация - теперь принимается объект, а не строка.

--- service-aggregation/src/main/java/ru/vpavlova/aggregation/config/KafkaConfig.java
+++ service-aggregation/src/main/java/ru/vpavlova/aggregation/config/KafkaConfig.java
@@ -1,18 +1,25 @@
 package ru.vpavlova.aggregation.config;
 
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.*;
-import org.springframework.kafka.listener.ContainerProperties.AckMode;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+import ru.vpavlova.aggregation.dto.IncomingEvent;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.springframework.kafka.listener.ContainerProperties.AckMode;
+
+@EnableKafka
 @Configuration
 public class KafkaConfig {
 
@@ -23,36 +30,40 @@ public class KafkaConfig {
     private String groupId;
 
     @Bean
-    public ProducerFactory<String, String> producerFactory() {
+    public ProducerFactory<String, Object> producerFactory() {
         Map<String, Object> props = new HashMap<>();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         props.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);
         return new DefaultKafkaProducerFactory<>(props);
     }
 
     @Bean
-    public KafkaTemplate<String, String> kafkaTemplate() {
+    public KafkaTemplate<String, Object> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
     }
 
     @Bean
-    public ConsumerFactory<String, String> consumerFactory() {
+    public ConsumerFactory<String, IncomingEvent> consumerFactory() {
+        JsonDeserializer<IncomingEvent> deserializer = new JsonDeserializer<>(IncomingEvent.class);
+        deserializer.addTrustedPackages("*");
+
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
-        return new DefaultKafkaConsumerFactory<>(props);
+
+        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), deserializer);
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory(ConsumerFactory<String, String> consumerFactory) {
-        ConcurrentKafkaListenerContainerFactory<String, String> factory =
+    public ConcurrentKafkaListenerContainerFactory<String, IncomingEvent> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, IncomingEvent> factory =
                 new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory);
+        factory.setConsumerFactory(consumerFactory());
         factory.getContainerProperties().setAckMode(AckMode.MANUAL_IMMEDIATE);
         return factory;
     }
--- service-aggregation/src/main/java/ru/vpavlova/aggregation/kafka/KafkaConsumerIdempotent.java
+++ service-aggregation/src/main/java/ru/vpavlova/aggregation/kafka/KafkaConsumerIdempotent.java
@@ -1,41 +1,36 @@
 package ru.vpavlova.aggregation.kafka;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import ru.vpavlova.aggregation.dto.IncomingEvent;
 import ru.vpavlova.aggregation.entity.ProcessedEvent;
 import ru.vpavlova.aggregation.repository.ProcessedEventRepository;
 import ru.vpavlova.aggregation.service.AggregationService;
 import ru.vpavlova.serviceaggregation.model.AggregatedServiceResponse;
 
-import java.util.Map;
-
 @Service
 @RequiredArgsConstructor
 public class KafkaConsumerIdempotent {
+
     private final ProcessedEventRepository processedMessageRepository;
     private final AggregationService aggregationService;
-    private final ObjectMapper objectMapper;
 
-    @KafkaListener(topics = "${kafka.consumer-topic}", groupId = "${kafka.group-id}")
-    public void consume(String payload) throws JsonProcessingException {
-        Map<String, String> messageMap = objectMapper.readValue(payload, new TypeReference<>() {
-        });
-        String id = messageMap.get("id");
-        String param = messageMap.get("param");
-
-        if (processedMessageRepository.existsById(id)) {
+    @KafkaListener(
+            topics = "${kafka.consumer-topic}",
+            groupId = "${kafka.group-id}",
+            containerFactory = "kafkaListenerContainerFactory"
+    )
+    public void consume(IncomingEvent event) {
+        if (processedMessageRepository.existsById(event.getId())) {
             return;
         }
 
-        AggregatedServiceResponse response = aggregationService.aggregateData(param).block();
-        System.out.println("Aggregated response for param " + param + ": " + response);
+        AggregatedServiceResponse response = aggregationService.aggregateData(event.getParam()).block();
+        System.out.println("Aggregated response for param " + event.getParam() + ": " + response);
 
         ProcessedEvent processed = new ProcessedEvent();
-        processed.setEventId(id);
+        processed.setEventId(event.getId());
         processedMessageRepository.save(processed);
     }
 }
