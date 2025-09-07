eda129eb134626c011f08b95547c094de3693b89
Victoria Pavlova <vikkkapaw@mail.ru>
2025-09-07 18:47:46 +0300

    Добавлена реализация Circuit Breaker (без класса конфигурации).

--- service-aggregation/src/main/java/ru/vpavlova/aggregation/service/AggregationService.java
+++ service-aggregation/src/main/java/ru/vpavlova/aggregation/service/AggregationService.java
@@ -1,6 +1,8 @@
 package ru.vpavlova.aggregation.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
@@ -18,14 +20,29 @@ public class AggregationService {
     private final OutboxRepository outboxRepository;
     private final ObjectMapper objectMapper;
 
-    public AggregatedServiceResponse aggregateData(String param) {
+    @CircuitBreaker(name = "firstService", fallbackMethod = "firstServiceFallback")
+    public String callFirstService(String param) {
         var client = restTemplateBuilder.build();
+        return client.getForObject(properties.getFirstServiceUrl() + "?param=" + param, String.class);
+    }
 
-        var firstServiceResponse = client.getForObject(
-                properties.getFirstServiceUrl() + "?param=" + param, String.class);
+    @CircuitBreaker(name = "secondService", fallbackMethod = "secondServiceFallback")
+    public String callSecondService(String param) {
+        var client = restTemplateBuilder.build();
+        return client.getForObject(properties.getSecondServiceUrl() + "?param=" + param, String.class);
+    }
 
-        var secondServiceResponse = client.getForObject(
-                properties.getSecondServiceUrl() + "?param=" + param, String.class);
+    private String firstServiceFallback(String param, Throwable ex) {
+        return "Default data from FIRST service for param " + param;
+    }
+
+    private String secondServiceFallback(String param, Throwable ex) {
+        return "Default data from SECOND service for param " + param;
+    }
+
+    public AggregatedServiceResponse aggregateData(String param) {
+        var firstServiceResponse = callFirstService(param);
+        var secondServiceResponse = callSecondService(param);
 
         if (firstServiceResponse == null && secondServiceResponse == null) {
             return null;
@@ -39,7 +56,7 @@ public class AggregationService {
             var outbox = new OutboxEvent();
             outbox.setPayload(objectMapper.writeValueAsString(response));
             outboxRepository.save(outbox);
-        } catch (Exception e) {
+        } catch (JsonProcessingException e) {
             throw new RuntimeException("Ошибка при сохранении события в outbox", e);
         }
 
